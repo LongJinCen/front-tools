440e7f7007699dc0bc1b147ae169069da726fefc
longjincen.try <longjincen.try@bytedance.com>
2021-10-04 01:39:32 +0800

    feat: 自定义并覆盖部分规则

--- packages/platform_eslint/src/config/_typescript.ts
+++ packages/platform_eslint/src/config/_typescript.ts
@@ -1,57 +1,80 @@
 export = {
-  plugins: [
-    '@typescript-eslint'
-  ],
+  plugins: ["@typescript-eslint"],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'airbnb-base',
-    'airbnb-typescript/base'
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "airbnb-base",
+    "airbnb-typescript/base",
   ],
   env: {
     jest: true,
-    es2021: true
+    es2021: true,
   },
   globals: {
-    flushPromises: 'readonly'
+    flushPromises: "readonly",
   },
-  parser:  '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ['./tsconfig.json'],
-    extraFileExtensions: ['.vue'],
+    project: ["./tsconfig.json"],
+    extraFileExtensions: [".vue"],
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: "module",
   },
   rules: {
-    'no-unsafe-optional-chaining': ['error'],
+    "no-unsafe-optional-chaining": ["error"],
     // 不能使用 any
-    '@typescript-eslint/no-explicit-any': ['error'],
+    "@typescript-eslint/no-explicit-any": ["error"],
     // 不允许使用 ts-ignore
-    '@typescript-eslint/ban-ts-comment': 'error',
-    '@typescript-eslint/naming-convention': [
-      'error',
+    "@typescript-eslint/ban-ts-comment": "error",
+    // interface、type、enum 命名规则
+    "@typescript-eslint/naming-convention": [
+      "error",
       {
-        selector: 'interface',
-        format: ['PascalCase'],
-        prefix: ['I']
+        selector: "interface",
+        format: ["PascalCase"],
+        prefix: ["I"],
       },
       {
-        selector: 'typeAlias',
-        format: ['PascalCase'],
-        prefix: ['T']
+        selector: "typeAlias",
+        format: ["PascalCase"],
+        prefix: ["T"],
       },
       {
-        selector: 'enum',
-        format: ['PascalCase'],
-        suffix: ['Enum']
-      }
+        selector: "enum",
+        format: ["PascalCase"],
+        suffix: ["Enum"],
+      },
     ],
-    'vue/max-attributes-per-line': [2, {
-      singleline: 20,
-      'multiline': {
-        max: 1,
-        allowFirstLine: false
-      }
-    }]
-  }
-}
+    "vue/max-attributes-per-line": [
+      2,
+      {
+        singleline: 20,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
+    // 复写 airbnb-base 插件内的该配置，不允许给函数的 params 重新复制，但可以修改其属性
+    "no-param-reassign": ["error", { props: false }],
+    // 复写 airbnb-base 插件内的该配置
+    "import/prefer-default-export": "off",
+    // 复写 airbnb-typescript/base 插件内的该配置
+    "@typescript-eslint/no-unused-expressions": [
+      "error",
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: false,
+      },
+    ],
+    // 复写 airbnb-base 插件内的该配置
+    "no-nested-ternary": "off",
+    // 复写 airbnb-base 插件内的该配置
+    "no-underscore-dangle": "off",
+    // 复写 @typescript-eslint/recommended-requiring-type-checking 中的该规则
+    "@typescript-eslint/no-floating-promises": ["error", { ignoreVoid: true }],
+    // 复写 airbnb-base 插件内的该配置
+    "no-void": ["error", { allowAsStatement: true }],
+  },
+};
--- packages/platform_eslint/src/config/_vue3.ts
+++ packages/platform_eslint/src/config/_vue3.ts
@@ -1,39 +1,49 @@
 export = {
-  plugins: [
-    'vue'
-  ],
-  extends: [
-    'plugin:vue/vue3-recommended',
-  ],
+  plugins: ["vue"],
+  extends: ["plugin:vue/vue3-recommended"],
   env: {
     browser: true,
     jest: true,
-    es2021: true
+    es2021: true,
   },
   globals: {
-    defineProps: 'readonly',
-    defineEmits: 'readonly',
-    defineExpose: 'readonly',
-    withDefaults: 'readonly',
-    hljs: 'readonly',
-    flushPromises: 'readonly'
+    defineProps: "readonly",
+    defineEmits: "readonly",
+    defineExpose: "readonly",
+    withDefaults: "readonly",
+    hljs: "readonly",
+    flushPromises: "readonly",
   },
-  parser: 'vue-eslint-parser',
+  parser: "vue-eslint-parser",
   parserOptions: {
-    parser: '@typescript-eslint/parser',
-    extraFileExtensions: ['.vue'],
-    project: ['./tsconfig.json'],
+    parser: "@typescript-eslint/parser",
+    extraFileExtensions: [".vue"],
+    project: ["./tsconfig.json"],
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: "module",
   },
   rules: {
-    'vue/component-definition-name-casing': ['error', 'kebab-case'],
-    'vue/max-attributes-per-line': [2, {
-      singleline: 20,
-      'multiline': {
-        max: 1,
-        allowFirstLine: false
-      }
-    }]
-  }
-}
+    "vue/component-definition-name-casing": ["error", "kebab-case"],
+    "vue/max-attributes-per-line": [
+      2,
+      {
+        singleline: 20,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
+    overrides: [
+      {
+        files: ["*.vue"],
+        rules: {
+          // 复写 airbnb-typescript/base 插件内的该配置，缺少对 .vue 文件的配置
+          "import/no-unresolved": "off",
+        },
+      },
+    ],
+    // 复写 airbnb-base 插件内的该配置, vue3 解构会失去响应式
+    "prefer-destructuring": ["off"],
+  },
+};
